ace3e6f2177c182917d7f62e975966c68a1332fa
stackexploit <stackexploit@protonmail.ch>
2018-02-07 01:23:41 +0100

    rename server.js

--- src/electron/server.ts
+++ src/electron/server.ts
@@ -9,15 +9,24 @@ export default function createApp(app) {
         res.send("hello");
     });
 
-    server.get("/test", (req, res) => {
+    server.get("/search", (req, res) => {
         res.setHeader("Content-Type", "application/json");
         anilist.init().then(() => {
-            anilist.search_anime(req.param("search")).then((result) => {
+            anilist.search_anime(req.param("title")).then((result) => {
                 res.send(JSON.stringify(result));
             });
         });
     });
 
+    server.get("/details", (req, res) => {
+        res.setHeader("Content-Type", "application/json");
+        anilist.init().then(() => {
+            anilist.get_anime_details(req.param("id")).then((result) => {
+                res.send(JSON.stringify(result));
+            });
+        });
+    });
+    
     server.listen(8080, () => {
         console.log("Server listening on port 8080");
     });
